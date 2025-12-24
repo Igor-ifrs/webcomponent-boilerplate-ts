a8a1bbe115c31c9a6bb28164ae29289582d79742
igor-ifrs <cigor80@gmail.com>
2025-12-23 22:19:56 -0300

    regex fix

--- src/javascript/components/ComponentName/_componentNameTemplate.ts
+++ src/javascript/components/ComponentName/_componentNameTemplate.ts
@@ -1,6 +1,6 @@
 const template = document.createElement("template");
 template.innerHTML = `
- <h1>###T</h1>
+ <h1>###</h1>
  <button id="action-btn" type="button">ACTION</button>
  </br>
  <span></span>
